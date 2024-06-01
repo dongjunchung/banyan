4275b91fcc2290c6324437814b5ee3b16b181ad0
Kyeong Joo Jung <iamkj03@naver.com>
2024-06-01 18:57:47 -0400

    Update on BANYAN package

---
+++ R/banyan.R
@@ -0,0 +1,13 @@
+#' mypackage: A package for fitting single and multilevel SBMs.
+#'
+#' This package fits Bayesian stochastic block models (SBMs)
+#'  
+#' @section banyan functions:
+#' The banyan functions ...
+#'
+#' @docType package
+#' @name banyan
+#' @useDynLib banyan
+#' @import Rcpp
+NULL
+#> NULL
